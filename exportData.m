abe84c31578c2c4be6b66e582f6929db8d8a3650
Iacopo Mochi <mochi.iacopo@gmail.com>
2022-08-02 14:38:48 +0200

    Export data improvement

--- exportData.m
+++ exportData.m
@@ -3,10 +3,21 @@ function exportData(app)
 D = app.dataTable.Data;
 Dc = app.dataTable_Contacts.Data;
 DS = app.ExportdataButton.UserData;
-DSc =  struct();
-DSc.PSD_LWR = DS.PSD_LWR;
-DSc.PSD_LWR_fit = DS.PSD_LWR_fit;
-DSc.PSD_LWR_fit_unbiased = DS.PSD_LWR_fit_unbiased;
+Ddata =  struct();
+Ddata.PSD_LWR = DS.PSD_LWR;
+Ddata.PSD_LWR_fit = DS.PSD_LWR_fit;
+Ddata.PSD_LWR_fit_unbiased = DS.PSD_LWR_fit_unbiased;
+Ddata.PSD_LERl = DS.PSD_LERl;
+Ddata.PSD_LERl_fit = DS.PSD_LERl_fit;
+Ddata.PSD_LERl_fit_unbiased = DS.PSD_LERl_fit_unbiased;
+Ddata.PSD_LERt = DS.PSD_LERt;
+Ddata.PSD_LERt_fit = DS.PSD_LERt_fit;
+Ddata.PSD_LERt_fit_unbiased = DS.PSD_LERt_fit_unbiased;
+Ddata.PS = DS.PS;
+Ddata.LeadingEdges = DS.ProfilesLFilled;
+Ddata.TrailingEdges = DS.ProfilesRFilled;
+Ddata.LineCenters = DS.LinesCenters;
+Ddata.Frequency = DS.freq;
 
 D = cell2table(D);
 Dc = cell2table(Dc);
@@ -89,9 +100,13 @@ pd = strfind(filename,'.');
 
 FN_Lines = [filename(1:pd-1) '_Lines' filename(pd:end)];
 FN_Contacts = [filename(1:pd-1) '_Contacts' filename(pd:end)];
-if strcmpi(FN_Lines(end-2:end),'mat')
-    save([pathname FN_Lines],'D','DSc')
+if strcmpi(filename(end-2:end),'mat')
+    save([pathname filename],'D','Dc','Ddata')
 else
-    writetable(D,[pathname FN_Lines])
-    writetable(Dc,[pathname FN_Contacts])
+    if ~isempty(D)
+        writetable(D,[pathname FN_Lines])
+    end
+    if ~isempty(Dc)
+        writetable(Dc,[pathname FN_Contacts])
+    end
 end
