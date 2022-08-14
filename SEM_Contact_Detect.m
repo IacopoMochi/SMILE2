86b7787754b43e206aef5c5f204cc1ca2082df5f
Iacopo Mochi <mochi.iacopo@gmail.com>
2022-08-15 00:13:08 +0200

    CrashProblem With contacts

--- SEM_Contact_Detect.m
+++ SEM_Contact_Detect.m
@@ -1,51 +1,51 @@
 function Output = SEM_Contact_Detect(parameters,app)
 
-filename = parameters.filename;
+%filename = parameters.filename;
 Output = struct();
-if strcmp(parameters.SEM_model,'Zeiss')
-    %Zeiss SEM: reading image pixel size from header
-    A = imfinfo(filename);
-    v = A.UnknownTags.Value;
-    r = strfind(v,char(13));
-    for n = 1:(length(r)-1)
-        s = v(r(n)+1:r(n+1)-1);
-        if contains(s,'Image Pixel Size = ')
-            ids1 = strfind(s,'Image Pixel Size = ');
-            s = s(ids1(1)+19:end);
-            ids = strfind(s,' ');
-            sc = s(1:ids(1)-1);
-            unit = s(ids(1)+1:end);
-            switch unit
-                case 'um'
-                    ps = str2double(sc)*1000;
-                case 'nm'
-                    ps = str2double(sc);
-                case 'pm'
-                    ps = str2double(sc)/1000;
-            end
-            parameters.ps = ps;
-            app.PixelsizenmEditField.Value = ps;
-        end
-    end
-elseif strcmp(parameters.SEM_model,'Hitachi')
-    %Hitachi SEM: reading image pixel size from header
-    A = imfinfo(filename);
-    v = A.UnknownTags.Value;
-    vv = v(v~=0); %remove spaces
-    r = find(vv==13); %detect line breaks
-    ps = [];
-    for n = 1:(length(r)-1)
-        s = char(vv(r(n)+1:r(n+1)-1));
-        if contains(s,'PixelSize=')
-            ps = str2double(s(12:end));
-            parameters.ps = ps;
-            app.PixelsizenmEditField.Value = ps;
-        end
-    end
-else
-    ps = parameters.ps;
-end
-
+% if strcmp(parameters.SEM_model,'Zeiss')
+%     %Zeiss SEM: reading image pixel size from header
+%     A = imfinfo(filename);
+%     v = A.UnknownTags.Value;
+%     r = strfind(v,char(13));
+%     for n = 1:(length(r)-1)
+%         s = v(r(n)+1:r(n+1)-1);
+%         if contains(s,'Image Pixel Size = ')
+%             ids1 = strfind(s,'Image Pixel Size = ');
+%             s = s(ids1(1)+19:end);
+%             ids = strfind(s,' ');
+%             sc = s(1:ids(1)-1);
+%             unit = s(ids(1)+1:end);
+%             switch unit
+%                 case 'um'
+%                     ps = str2double(sc)*1000;
+%                 case 'nm'
+%                     ps = str2double(sc);
+%                 case 'pm'
+%                     ps = str2double(sc)/1000;
+%             end
+%             parameters.ps = ps;
+%             app.PixelsizenmEditField.Value = ps;
+%         end
+%     end
+% elseif strcmp(parameters.SEM_model,'Hitachi')
+%     %Hitachi SEM: reading image pixel size from header
+%     A = imfinfo(filename);
+%     v = A.UnknownTags.Value;
+%     vv = v(v~=0); %remove spaces
+%     r = find(vv==13); %detect line breaks
+%     ps = [];
+%     for n = 1:(length(r)-1)
+%         s = char(vv(r(n)+1:r(n+1)-1));
+%         if contains(s,'PixelSize=')
+%             ps = str2double(s(12:end));
+%             parameters.ps = ps;
+%             app.PixelsizenmEditField.Value = ps;
+%         end
+%     end
+% else
+%     ps = parameters.ps;
+% end
+ps = parameters.ps;
 
 A = double(parameters.rawImages);
 
@@ -153,8 +153,8 @@ ContoursRadius = ContoursRadius2;
 
 AllRadii = cell(numel(MCx),1);
 for n = 1:numel(MCx)
-    ContoursCentersX(n) = nanmean(MCx{n});
-    ContoursCentersY(n) = nanmean(MCy{n});
+    ContoursCentersX(n) = mean(MCx{n},'omitnan');
+    ContoursCentersY(n) = mean(MCy{n},'omitnan');
     AllRadii{n} = sqrt((MCx{n}-ContoursCentersX(n)).^2+...
         (MCy{n}-ContoursCentersY(n)).^2);
     ContoursRadius(n) = mean(AllRadii{n});
